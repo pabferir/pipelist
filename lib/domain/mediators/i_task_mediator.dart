bbee1ca9b5f451a4b745061a68747f6709a5d167
Pablo <pablo_f.i@hotmail.com>
2021-08-29 18:38:39 +0200

    Refactored mediator class

--- lib/domain/mediators/i_task_mediator.dart
+++ lib/domain/mediators/i_task_mediator.dart
@@ -1,6 +1,6 @@
 import 'package:pipelist/domain/entities/task_entity.dart';
 
-abstract class TaskUseCases {
+abstract class ITaskMediator {
   Future<void> createTask();
   Stream<List<TaskEntity>> readAllTasks();
   Stream<List<TaskEntity>> readUncompletedTasks();
